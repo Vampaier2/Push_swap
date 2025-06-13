512a19eb6e94b07e147f6bc099230d76eed01bf6
xalves <92018803+Vampaier2@users.noreply.github.com>
2025-06-13 15:19:27 +0100

    Add files via upload

---
+++ push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xalves <xalves@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 11:52:27 by xalves            #+#    #+#             */
+/*   Updated: 2025/06/13 15:14:04 by xalves           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "Bonus_ButBetter/libft.h"
+
+long	ft_atoi(const char *nptr);
+
+#endif
--- sandbox.c
+++ sandbox.c
@@ -6,17 +6,16 @@
 /*   By: xalves <xalves@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 12:05:09 by xalves            #+#    #+#             */
-/*   Updated: 2025/06/10 17:23:22 by xalves           ###   ########.fr       */
+/*   Updated: 2025/06/13 15:13:30 by xalves           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	get_last_binary_digit(int num)
+#include "push_swap.h"
+
+/* int	get_last_binary_digit(int num)
 {
 	return (num & 1); // Bitwise AND with 1 extracts the last binary digit
-}
-
-#include <stdio.h>
-
+} */
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
@@ -43,9 +42,58 @@ void	ft_sort_int_tab(int *tab, int size)
 	}
 }
 
-int	main(void)
+int	main(int argc, char const *argv[])
 {
-	int	num[] = {7, 13, 2, 56, 124, 5, 82, 1};
+	int		i;
+	long	content_aux;
+	t_list	*og_list;
+	int		*aux_array;
+
+	i = 1;
+	og_list = NULL;
+	aux_array = malloc((argc - 1) * sizeof(int));
+	if (!aux_array)
+		return (printf("Memory allocation failed!\n"), 1);
+	while (i < argc)
+	{
+		content_aux = ft_atoi(argv[i]);
+		if (content_aux == LONG_MAX)
+			return (printf("Invalid Number!!!"), 0);
+		ft_lstadd_back(&og_list, ft_lstnew(content_aux));
+		aux_array[i - 1] = content_aux;
+		i++;
+	}
+	print_list(og_list);
+	printf("\n\n");
+	printf("Sort List:\n");
+	ft_sort_int_tab(aux_array, argc - 1);
+	i = 1;
+	while (i < argc)
+	{
+		printf("[%d]", aux_array[i - 1]);
+		i++;
+	}
+	return (0);
+}
+
+/* int	main(void)
+{
+	t_list	*head;
+
+	head = NULL;
+	ft_lstadd_back(&head, ft_lstnew());
+	ft_lstadd_back(&head, ft_lstnew());
+	ft_lstadd_back(&head, ft_lstnew());
+	printf("Before clear:\n");
+	print_list(head);
+	ft_lstclear(&head);
+	printf("After clear:\n");
+	print_list(head);
+	return (0);
+} */
+/* int	main(void)
+{
+	int	num[] = {23, 4, 15, 8, 42, 16, 7, 0, 3, 31};
 	int	i;
 
 	i = 0;
@@ -54,12 +102,7 @@ int	main(void)
 	printf("\nSize -> |%d|\n", size);
 	while (i < size)
 	{
-		if (get_last_binary_digit(num[i]) == 0)
-			printf("Push to B (pb) |%d|->|%d|\n", num[i],
-				get_last_binary_digit(num[i]));
-		else
-			printf("Rotate A (ra) |%d|->|%d|\n", num[i],
-				get_last_binary_digit(num[i]));
+		printf("%d|", num[i]);
 		i++;
 	}
 	printf("\n");
@@ -83,4 +126,4 @@ int	main(void)
 		i++;
 	}
 	return (0);
-}
+} */
