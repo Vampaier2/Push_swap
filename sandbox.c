bffe6432d91735d8d1c147da59be6adddb6e2f90
xalves <92018803+Vampaier2@users.noreply.github.com>
2025-06-16 17:11:28 +0100

    Add files via upload

--- sandbox.c
+++ sandbox.c
@@ -6,11 +6,12 @@
 /*   By: xalves <xalves@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 12:05:09 by xalves            #+#    #+#             */
-/*   Updated: 2025/06/13 15:13:30 by xalves           ###   ########.fr       */
+/*   Updated: 2025/06/16 15:43:05 by xalves           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdlib.h>
 
 /* int	get_last_binary_digit(int num)
 {
@@ -42,15 +43,63 @@ void	ft_sort_int_tab(int *tab, int size)
 	}
 }
 
+int get_bit_length(int n)
+{
+	int length;
+
+	length = 0;
+	while (n > 0)
+	{
+		n >>= 1;
+		length++;
+	}
+	return (length == 0) ? 1 : length; // handle n == 0
+}
+
+char *int_to_binary(int n, int length)
+{
+	char *binary;
+	int i;
+
+	binary = malloc(length + 1);
+	if (!binary)
+		return NULL;
+	binary[length] = '\0';
+
+	i = 0;
+	while (i < length)
+	{
+		if (n & (1 << (length - 1 - i)))
+			binary[i] = '1';
+		else
+			binary[i] = '0';
+		i++;
+	}
+	return binary;
+}
+
+
+int	ft_get_idx(t_list *lst, int *sort_array)
+{
+	int	idx;
+
+	idx = 0;
+	while ((lst->content) != sort_array[idx])
+		idx++;
+	return (idx);
+}
+
 int	main(int argc, char const *argv[])
 {
 	int		i;
 	long	content_aux;
 	t_list	*og_list;
 	int		*aux_array;
+	int		M_idx;
 
 	i = 1;
 	og_list = NULL;
+	// Populate Array
 	aux_array = malloc((argc - 1) * sizeof(int));
 	if (!aux_array)
 		return (printf("Memory allocation failed!\n"), 1);
@@ -63,16 +112,28 @@ int	main(int argc, char const *argv[])
 		aux_array[i - 1] = content_aux;
 		i++;
 	}
+	M_idx = ft_lstsize(og_list) - 1;
 	print_list(og_list);
 	printf("\n\n");
-	printf("Sort List:\n");
+	// Sort Aray
+	printf("Sort List:");
 	ft_sort_int_tab(aux_array, argc - 1);
 	i = 1;
 	while (i < argc)
 	{
-		printf("[%d]", aux_array[i - 1]);
+		printf(" [%d] ", aux_array[i - 1]);
 		i++;
 	}
+	// Find and Replace
+	printf("\n\nIndexed List:\n");
+	while (og_list)
+	{
+		og_list->content = ft_get_idx(og_list, aux_array);
+		og_list->binary = int_to_binary((og_list->content), get_bit_length(M_idx));
+		printf("[%d]->[%s]\n", (og_list->content), (og_list->binary));
+		og_list = og_list->next;
+	}
+	printf("\nBigger idx = %d\n\n", M_idx);
 	return (0);
 }
 
