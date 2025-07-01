4462c5af66d0a34cdef2e5c6e8fd32dd535df5c7
xalves <92018803+Vampaier2@users.noreply.github.com>
2025-07-01 13:15:06 +0100

    Add files via upload
    Push_swap only missing sort5().

---
+++ Push_swap/ft_sorts.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sorts.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xalves <xalves@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 12:06:34 by xalves            #+#    #+#             */
+/*   Updated: 2025/07/01 13:10:36 by xalves           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	j = 0;
+	size = size - 1;
+	while (i < size)
+	{
+		while (j < size + 1)
+		{
+			if (tab[i] > tab[j])
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+		j = i;
+	}
+}
